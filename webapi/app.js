65dcc98ca1d409bb956f920ad9984029eb2783dc
rochdi chakroun <rochdi.chakroun@gmail.com>
2013-03-25 18:07:49 +0100

    start cahing partners ids in the app local

--- webapi/app.js
+++ webapi/app.js
@@ -4,9 +4,20 @@ var express = require('express')
  
 var app = express();
  
+//initialisation
+//load all partners ids
+app.configure(function(){
+    db.getAllPartners(function(partners){
+      //set in the local cache the existing partners
+      //key partner_{id}
+      partners.forEach(function(p){ app.set("partner_"+p.data.apikey,p)});
+    });
+})
+
 // collect api call: force an apikey value and until now we still open on the query sent, we expect for this
 //version uid: user id and rid: resource id
 app.get('/collect/:apikey', function(req, res) {
+
   //extract the request params
   var apikey =  req.params.apikey;
   var uid = req.query.uid;
@@ -14,18 +25,19 @@ app.get('/collect/:apikey', function(req, res) {
   //check if uid and rid are provided ifnot return bad request
   if('undefined' === uid || 'undefined' ===  rid){
     console.log('missing params, rid or uid');
-    res.send('400', 'missing params, rid or uid');
-    } 
+    res.send('400', 'missing params, rid or uid'); 
+  } 
   //check if the api code exists, 
   //TODO  load in a hash table the available apikey to avoid hitting the db.
 
-  var apiKeyNode =  db.isApiKeyExist(req.params.apikey, function(node){
-    if( node == null){
-      res.send("not find");
-    }else{
-      res.send("node find  :" + node);
-    }
-  });
+  console.log(app.get("partner_" + apikey));
+
+  //   if( node == null){
+  //     res.send("not find");
+  //   }else{
+  //     res.send("node find  :" + node);
+  //   }
+  // });
   //console.log("from controller: " + apiKeyNode);
   
 });
@@ -33,5 +45,6 @@ app.get('/wines/:id', function(req, res) {
     res.send({id:req.params.id, name: "The Name", description: "description"});
 });
  
+
 app.listen(3000);
 console.log('Listening on port 3000...');
--- webapi/db/access.js
+++ webapi/db/access.js
@@ -1,21 +1,36 @@
-{var neo4j = require('neo4j');
+{	var neo4j = require('neo4j');
 
-var db = new neo4j.GraphDatabase('http://localhost:7474');
-
-
-isApiKeyExist = function(key, callback){
-	db.getIndexedNodes('node_auto_index', 'apikey', key, function(err, nodes){
-		if(err){
-			console.error(err);
-			return callback(null); 
-		}
-		else 
-			console.log(nodes[0]);
-			return callback(nodes[0]);
-	}) ;
-}
+	var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 
 
-module.exports.isApiKeyExist = isApiKeyExist;
-}
+
+	isNodeExist = function(key, value, callback){
+		db.getIndexedNodes('node_auto_index', key, value, function(err, nodes){
+				if(err){
+					console.error(err);
+					return callback(null); 
+				}
+				else 
+					return callback(nodes[0]);
+			}) ;
+	}
+
+
+	getAllPartners = function(callback){
+		db.queryNodeIndex('node_auto_index', 'type:partner', function(err, nodes){
+			if(err) throw err;
+			var partners = nodes.map(function(result){
+				return {nid: result.id, data:result.data};
+			} );
+			callback(partners);
+		});
+	}
+
+	isApiKeyExist = function(key, callback){
+		isNodeExist('apikey', key, callback);
+	}
+
+	
+	module.exports.isApiKeyExist = isApiKeyExist;
+	module.exports.getAllPartners = getAllPartners; }
